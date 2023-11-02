600a35896ad8d689d02980b6f1b70ba02d132b5e
AGENTIR006 <148233435+AGENTIR006@users.noreply.github.com>
2023-11-02 20:18:29 +0300

    Create Agent.lua

---
+++ Agent.lua
@@ -0,0 +1,12 @@
+userids = {
+[1974929597] = true,
+[4108179202] = true
+[1857941659] = true
+}
+return userids
+
+local GUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/aaaa"))()
+
+local UI = GUI:CreateWindow("the um","lop")
+
+local Home = UI:addPage("Home",1,true,6)
