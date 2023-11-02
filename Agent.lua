fe8452c6d7f76b79cf53f66f90cdf01f353847d2
AGENTIR006 <148233435+AGENTIR006@users.noreply.github.com>
2023-11-02 20:22:20 +0300

    Update Agent.lua

--- Agent.lua
+++ Agent.lua
@@ -1,9 +1,4 @@
-userids = {
-[1974929597] = true,
-[4108179202] = true
-[1857941659] = true
-}
-return userids
+
 
 local GUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/aaaa"))()
 
